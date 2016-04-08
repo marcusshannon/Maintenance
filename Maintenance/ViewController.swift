9c7b23ad9faa47cf523e101c7ca8c6c746e48a26
marcusshannon <mpshannon@me.com>
2016-04-07 21:26:47 -0300

    First commit

---
+++ Maintenance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Maintenance
+//
+//  Created by Marcus Shannon on 4/7/16.
+//  Copyright © 2016 Metro Self Storage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
